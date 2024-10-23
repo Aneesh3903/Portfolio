b4bc4d1b79f47bcaeca3bcc4793dc31d2fd62fef
Anees prajapati <paneesh303@gmail.com>
2024-10-23 13:22:17 +0530

    First commit

---
+++ script.js
@@ -0,0 +1,30 @@
+
+let isFrontend = true;
+        function toggleText() {
+            const textElement = document.getElementsByClassName('text');
+            if (isFrontend) {
+                textElement.textContent = 'React Developer';
+            } else {
+                textElement.textContent = 'Frontend Developer';
+            }
+            isFrontend = !isFrontend;
+        }
+        setInterval(toggleText, 2000);
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if (top >= offset && top < offset + height) {
+            navLinks.forEach(link => {
+                link.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+        }
+    });
+};
